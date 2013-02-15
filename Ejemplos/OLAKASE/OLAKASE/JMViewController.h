4033afd1a9e8203742dcb1758e4c5337b0a880d1
Jorge Maroto <patoroco@gmail.com>
2013-02-15 21:46:30 +0100

    Primer ejemplo

---
+++ Ejemplos/OLAKASE/OLAKASE/JMViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMViewController.h
+//  OLAKASE
+//
+//  Created by Jorge Maroto Garcia on 15/2/13.
+//  Copyright (c) 2013 Tactilapp.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMViewController : UIViewController
+
+@end
---
+++ Ejemplos/OLAKASE/OLAKASE/JMViewController.m
@@ -0,0 +1,29 @@
+//
+//  JMViewController.m
+//  OLAKASE
+//
+//  Created by Jorge Maroto Garcia on 15/2/13.
+//  Copyright (c) 2013 Tactilapp.com. All rights reserved.
+//
+
+#import "JMViewController.h"
+
+@interface JMViewController ()
+
+@end
+
+@implementation JMViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
